3508e4e627819d7fea9a74c84656cc8187a5dc3c
Mela <pamela.gotti@instapartners.it>
2017-12-03 11:26:25 +0100

    * day03: Separating functions to get coordinates and distance

--- 2017/day03/manhattan.py
+++ 2017/day03/manhattan.py
@@ -10,29 +10,24 @@ def get_memory_position(memory_location):
     (min_square + n * (square_size - 1), min_square + (n + 1) * (square_size - 1) - 1)
     for n in range(0, 4)
   ]
-  coordinates = (0, 0, 1)
+  coordinates = (0, 0)
   half_arm = floor(square_size / 2)
   for i, (min_arm, max_arm) in enumerate(square_coordinates):
     if memory_location >= min_arm and memory_location <= max_arm:
       position_on_arm = memory_location - min_arm - half_arm + 1
       if i == 0:
-        coordinates = (half_arm, position_on_arm, square_size)
+        coordinates = (half_arm, position_on_arm)
       elif i == 1:
-        coordinates = (position_on_arm, half_arm, square_size)
+        coordinates = (position_on_arm, half_arm)
       elif i == 2:
-        coordinates = (-half_arm, position_on_arm, square_size)
+        coordinates = (-half_arm, position_on_arm)
       elif i == 3:
-        coordinates = (position_on_arm, half_arm, square_size)
+        coordinates = (position_on_arm, -half_arm)
       break
   return coordinates
-    
-  # distance = 0
-  # half_arm = floor(square_size / 2)
-  # for i, (min_arm, max_arm) in enumerate(square_coordinates):
-  #   if memory_location >= min_arm and memory_location <= max_arm:
-  #     distance = half_arm + memory_location - min_arm - half_arm + 1
-  #     break
-  # return distance
+
+def get_distance(x, y):
+  return abs(x) + abs(y)
 
 def manhattan_memory(memory_location):
-  return get_memory_position(memory_location)
+  return get_distance(*(get_memory_position(memory_location)))
--- 2017/day03/manhattan_test.py
+++ 2017/day03/manhattan_test.py
@@ -7,15 +7,14 @@ from manhattan import *
 class ManhattanTest(unittest.TestCase):
 
   @data(
-    (1, (0, 0, 1)),
-    (12, (2, 1, 5)),
-    (23, (0, -2, 5)),
-    (1024, 31),
-    (289326, 419)
+    (1, (0, 0)),
+    (12, (2, 1)),
+    (23, (0, -2)),
+    (1024, (15, 16)),
   )
   @unpack
   def test_get_memory_position(self, memory_location, expected_coordinates):
-    self.assertEqual(expected_coordinates, manhattan_memory(memory_location))
+    self.assertEqual(expected_coordinates, get_memory_position(memory_location))
 
   @data(
     (1, 0),
