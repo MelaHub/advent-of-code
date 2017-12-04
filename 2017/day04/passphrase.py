a79f3d74f4325e3180d7b92d828208f935325c81
Mela <pamela.gotti@instapartners.it>
2017-12-04 23:25:37 +0100

    * master: Added day-4 part II

--- 2017/day04/passphrase.py
+++ 2017/day04/passphrase.py
@@ -1,16 +1,26 @@
-def is_valid(passphrase):
+identity = lambda x: x
+sort_word = lambda x: ''.join(sorted(x))
+
+def _is_valid(passphrase, word_fn):
   cache = set()
   for word in passphrase.split(' '):
-    if word in cache:
+    new_word = word_fn(word)
+    if new_word in cache:
       return False
-    cache.add(word)
+    cache.add(new_word)
   return True
 
-def count_valid_passphrases(passphrases):
-  return sum([1 if is_valid(phrase) else 0 for phrase in passphrases])
+def is_valid_identity(passphrase):
+  return _is_valid(passphrase, identity)
 
-def count_valid_from_default_file():
+def is_valid_anagram(passphrase):
+  return _is_valid(passphrase, sort_word)
+
+def count_valid_passphrases(passphrases, is_valid_fn):
+  return sum([1 if is_valid_fn(phrase) else 0 for phrase in passphrases])
+
+def count_valid_from_default_file(is_valid_fn):
   passphrases = []
   with open('passphrases') as f:
     passphrases += [row.strip() for row in f.readlines()]
-  return count_valid_passphrases(passphrases)
+  return count_valid_passphrases(passphrases, is_valid_fn)
--- 2017/day04/passphrase_test.py
+++ 2017/day04/passphrase_test.py
@@ -12,12 +12,27 @@ class PassphraseTest(unittest.TestCase):
     ('aa bb cc dd aaa', True)
   )
   @unpack
-  def test_captcha(self, input_pass, expected_out):
-    self.assertEqual(expected_out, is_valid(input_pass))
+  def test_is_valid_identity(self, input_pass, expected_out):
+    self.assertEqual(expected_out, is_valid_identity(input_pass))
 
   def test_count_valid(self):
-    self.assertEqual(2, count_valid_passphrases(['aa bb cc dd ee', 'aa bb cc dd aa', 'aa bb cc dd aaa']))
+    self.assertEqual(2, count_valid_passphrases(['aa bb cc dd ee', 'aa bb cc dd aa', 'aa bb cc dd aaa'], is_valid_identity))
 
-  def test_default_file(self):
-    self.assertEqual(0, count_valid_from_default_file())
+  def test_default_file_identity(self):
+    self.assertEqual(477, count_valid_from_default_file(is_valid_identity))
+
+  def test_default_file_anagrem(self):
+    self.assertEqual(167, count_valid_from_default_file(is_valid_anagram))
+
+  @data(
+    ('abcde fghij', True),
+    ('abcde xyz ecdab', False),
+    ('a ab abc abd abf abj', True),
+    ('iiii oiii ooii oooi oooo', True),
+    ('oiii ioii iioi iiio', False)
+  )
+  @unpack
+  def test_is_valid_anagram(self, input_pass, expected_out):
+    self.assertEqual(expected_out, is_valid_anagram(input_pass))
+
 
