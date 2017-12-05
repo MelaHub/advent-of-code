4611752d4da01d333d34481b95517ce5f3cef85d
Mela <pamela.gotti@instapartners.it>
2017-12-05 23:58:44 +0100

    * master: Added second part of day05

--- 2017/day05/maze.py
+++ 2017/day05/maze.py
@@ -3,6 +3,14 @@ def move_in_maze(maze, curr_index):
   maze[curr_index] += 1
   return (maze, next_index)
 
+def move_weird_in_maze(maze, curr_index):
+  next_index = curr_index + maze[curr_index]
+  if maze[curr_index] >= 3:
+    maze[curr_index] -= 1
+  else:
+    maze[curr_index] += 1
+  return (maze, next_index)
+
 # The recursive solutino is not feasible bcs of max level of recursion reached
 def solve_maze_rec(maze, curr_index, number_of_moves):
   if curr_index < 0 or curr_index >= len(maze):
@@ -14,17 +22,17 @@ def solve_maze(maze):
   number_of_moves = solve_maze_rec(maze, 0, 0)
   return number_of_moves
 
-def solve_maze_non_rec(maze):
+def solve_maze_non_rec(maze, move_fn):
   curr_index = 0
   number_of_moves = 0
   while curr_index >= 0 and curr_index < len(maze):
-    maze, curr_index = move_in_maze(maze, curr_index)
+    maze, curr_index = move_fn(maze, curr_index)
     number_of_moves += 1
   return number_of_moves
 
-def solve_maze_from_file():
+def solve_maze_from_file(move_fn):
   rows = []
   with open('maze') as f:
     rows += [int(row) for row in f.readlines()]
-  return solve_maze_non_rec(rows)
+  return solve_maze_non_rec(rows, move_fn)
   
--- 2017/day05/maze_test.py
+++ 2017/day05/maze_test.py
@@ -6,6 +6,12 @@ from maze import *
 @ddt
 class PassphraseTest(unittest.TestCase):
 
+  def _move_and_check(self, input_values, expected_out, fn):
+    input_maze, current_index = input_values
+    expected_maze, expected_index = expected_out
+    actual_maze, actual_index = fn(input_maze, current_index)
+    self.assertListEqual(expected_maze, actual_maze) and self.assertEqual(expected_index, current_index)
+
   @data(
     (([0, 3, 0, 1, -3], 0), ([1, 3, 0, 1, -3], 0)),
     (([1, 3, 0, 1, -3], 0), ([2, 3, 0, 1, -3], 1)),
@@ -15,13 +21,27 @@ class PassphraseTest(unittest.TestCase):
   )
   @unpack
   def test_is_valid_identity(self, input_values, expected_out):
-    input_maze, current_index = input_values
-    expected_maze, expected_index = expected_out
-    actual_maze, actual_index = move_in_maze(input_maze, current_index)
-    self.assertListEqual(expected_maze, actual_maze) and self.assertEqual(expected_index, current_index)
+    self._move_and_check(input_values, expected_out, move_in_maze)
+
+  @data(
+    (([0, 3, 0, 1, -3], 0), ([1, 3, 0, 1, -3], 0)),
+    (([1, 3, 0, 1, -3], 0), ([2, 3, 0, 1, -3], 1)),
+    (([2, 3, 0, 1, -3], 1), ([2, 2, 0, 1, -3], 4)),
+    (([2, 2, 0, 1, -3], 4), ([2, 2, 0, 1, -2], 1)),
+    (([2, 2, 0, 1, -2], 1), ([2, 3, 0, 1, -2], 3)),
+  )
+  @unpack
+  def test_is_valid_identity(self, input_values, expected_out):
+    self._move_and_check(input_values, expected_out, move_weird_in_maze)
 
   def test_solve_maze_works(self):
-    self.assertEqual(5, solve_maze([0, 3, 0, 1, -3]))
+    self.assertEqual(5, solve_maze_non_rec([0, 3, 0, 1, -3], move_in_maze))
+
+  def test_solve_weird_maze_works(self):
+    self.assertEqual(10, solve_maze_non_rec([0, 3, 0, 1, -3], move_weird_in_maze))
 
   def test_solve_maze_from_file_works(self):
-    self.assertEqual(0, solve_maze_from_file())
+    self.assertEqual(372139, solve_maze_from_file(move_in_maze))
+
+  def test_solve_weird_maze_from_file_works(self):
+    self.assertEqual(29629538, solve_maze_from_file(move_weird_in_maze))
