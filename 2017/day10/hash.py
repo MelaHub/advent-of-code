498d2540f7cbfe203f09d2074fe0de0912d4491f
Mela <pamela.gotti@instapartners.it>
2017-12-20 22:56:06 +0100

    * day10: Less swaps

--- 2017/day10/hash.py
+++ 2017/day10/hash.py
@@ -3,15 +3,19 @@ def reverse_elements(input_list, current_position, length):
   end_position = current_position + length
   if end_position >= len(input_list):
     end_position -= len(input_list)
-  i = current_position
-  j = end_position - 1
-  while i != j - 1 or i != j - 2 or not (i == 0 and j == len(input_list) - 1):
-    copy_input[i], copy_input[j] = copy_input[j], copy_input[i]
+  i = current_position - 1
+  j = end_position
+  start_copy = False
+  while i != j - 2:
     i += 1
     j -= 1
     if i == len(input_list):
       i = 0
     if j == -1:
       j = len(input_list) - 1
+    if start_copy and (i == 0 and j == len(input_list) - 1 or i == j + 1):
+      break
+    copy_input[i], copy_input[j] = copy_input[j], copy_input[i]
+    start_copy = True
   return copy_input
     
--- 2017/day10/hash_test.py
+++ 2017/day10/hash_test.py
@@ -10,6 +10,7 @@ class HashTest(unittest.TestCase):
     ([0, 1, 2, 3, 4], 0, 3, [2, 1, 0, 3, 4]),
     ([2, 1, 0, 3, 4], 3, 4, [4, 3, 0, 1, 2]),
     ([4, 3, 0, 1, 2], 1, 5, [3, 4, 2, 1, 0]),
+    ([3, 4, 2, 1, 0], 1, 4, [3, 0, 1, 2, 4]),
   )
   @unpack
   def test_reverse_elements(self, input_list, current_position, reverse_length, expected_output):
