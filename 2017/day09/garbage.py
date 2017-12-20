d68cec91e09ef0f7f3c67e7a9aa801fff3e55c71
Mela <pamela.gotti@instapartners.it>
2017-12-20 19:53:33 +0100

    * day09: second part of garbage

--- 2017/day09/garbage.py
+++ 2017/day09/garbage.py
@@ -1,19 +1,63 @@
+OPEN_GARBAGE = '<'
+CLOSE_GARBAGE = '>'
+OPEN_GROUP = '{'
+CLOSE_GROUP = '}'
+IGNORE_NEXT = '!'
+
+def find_groups_in_string(input_groups):
+  groups = []
+  garbage = []
+  group_stacks = []
+  inside_garbage = False
+  i = 0
+  while i < len(input_groups):
+    if input_groups[i] == IGNORE_NEXT:
+      garbage[-1] += '%s%s' % (input_groups[i], input_groups[i+1])
+      i += 2
+      continue
+    if input_groups[i] == OPEN_GARBAGE and not inside_garbage:
+      inside_garbage = True
+      garbage += input_groups[i]
+    elif input_groups[i] == CLOSE_GARBAGE and inside_garbage:
+      garbage[-1] += input_groups[i]
+      inside_garbage = False
+    elif input_groups[i] == OPEN_GROUP and not inside_garbage:
+      group_stacks += input_groups[i]
+    elif input_groups[i] == CLOSE_GROUP and not inside_garbage:
+      group_stacks[-1] += input_groups[i]
+      full_group = group_stacks[-1]
+      groups += ((full_group, len(group_stacks)),)
+      group_stacks = group_stacks[:-1]
+      if len(group_stacks):
+        group_stacks[-1] += full_group
+    elif len(group_stacks):
+      if inside_garbage:
+        garbage[-1] += input_groups[i]
+      else:
+        group_stacks[-1] += input_groups[i]
+    i += 1
+  return groups, garbage
+
 def get_score_for_group(input_groups):
+  groups, garbage = find_groups_in_string(input_groups)
+  return sum([score for _, score in groups])
+
+def get_score_for_group2(input_groups):
   curr_score = 0
   number_of_nested_groups = 0
   inside_garbage = False
   i = 0
   while i < len(input_groups):
-    if input_groups[i] == '!':
+    if input_groups[i] == IGNORE_NEXT:
       i += 2
       continue
-    if input_groups[i] == '<':
+    if input_groups[i] == OPEN_GARBAGE:
       inside_garbage = True
-    elif input_groups[i] == '>':
+    elif input_groups[i] == CLOSE_GARBAGE:
       inside_garbage = False
-    elif input_groups[i] == '{' and not inside_garbage:
+    elif input_groups[i] == OPEN_GROUP and not inside_garbage:
       number_of_nested_groups += 1
-    elif input_groups[i] == '}' and not inside_garbage:
+    elif input_groups[i] == CLOSE_GROUP and not inside_garbage:
       curr_score += number_of_nested_groups
       number_of_nested_groups -= 1
     i += 1
@@ -24,3 +68,27 @@ def get_score_from_file():
   with open('garbage') as f:
     groups += ''.join([row for row in f.readlines()])
   return get_score_for_group(groups)
+
+def clean_garbage(word):
+  clean_word = ''
+  i = 0
+  if len(word) < 2:
+    return word
+  word = word[1:-1]
+  while i < len(word):
+    if word[i] == IGNORE_NEXT:
+      i += 1
+    else:
+      clean_word += word[i]
+    i += 1
+  return clean_word
+ 
+def count_valid_chars(word):
+  return len(clean_garbage(word)) 
+
+def get_num_non_cancelled_from_file():
+  input_groups = ''
+  with open('garbage') as f:
+    input_groups += ''.join([row for row in f.readlines()])
+  _, garbage = find_groups_in_string(input_groups)
+  return sum([count_valid_chars(word) for word in garbage])
--- 2017/day09/garbage_test.py
+++ 2017/day09/garbage_test.py
@@ -7,6 +7,22 @@ from garbage import *
 class RegisterTest(unittest.TestCase):
 
   @data(
+    ('{}', [('{}', 1)], []),
+    ('{{{}}}', [('{}', 3), ('{{}}', 2), ('{{{}}}', 1)], []),
+    ('{{},{}}', [('{}', 2), ('{}', 2), ('{{},{}}', 1)], []),
+    ('{{{},{},{{}}}}', [('{}', 3), ('{}', 3), ('{}', 4), ('{{}}', 3), ('{{},{},{{}}}', 2), ('{{{},{},{{}}}}', 1)], []),
+    ('{<a>,<a>,<a>,<a>}', [('{,,,}', 1)], ['<a>', '<a>', '<a>', '<a>']),
+    ('{{<ab>},{<ab>},{<ab>},{<ab>}}', [('{}', 2), ('{}', 2), ('{}', 2), ('{}', 2), ('{{},{},{},{}}', 1)], ['<ab>', '<ab>', '<ab>', '<ab>']),
+    ('{{<!!>},{<!!>},{<!!>},{<!!>}}', [('{}', 2), ('{}', 2), ('{}', 2), ('{}', 2), ('{{},{},{},{}}', 1)], ['<!!>', '<!!>', '<!!>', '<!!>']),
+    ('{{<a!>},{<a!>},{<a!>},{<ab>}}', [('{}', 2), ('{{}}', 1)], ['<a!>},{<a!>},{<a!>},{<ab>']),
+  )
+  @unpack
+  def test_find_groups(self, input_groups, expected_groups, expected_garbage):
+    groups, garbage = find_groups_in_string(input_groups)
+    self.assertEqual(expected_groups, groups)
+    self.assertEqual(expected_garbage, garbage)
+
+  @data(
     ('{}', 1),
     ('{{{}}}', 6),
     ('{{},{}}', 5),
@@ -21,4 +37,22 @@ class RegisterTest(unittest.TestCase):
     self.assertEqual(expected_score, get_score_for_group(input_groups))
 
   def test_parse_file(self):
-    self.assertEqual(0, get_score_from_file())
+    self.assertEqual(12396, get_score_from_file())
+
+  def test_num_char(self):
+    self.assertEqual(0, get_num_non_cancelled_from_file())
+
+  @data(
+    ('<>', 0),
+    ('<random characters>', 17),
+    ('<<<<>', 3),
+    ('<{!>}>', 2),
+    ('<!!>', 0),
+    ('<!!!>>', 0),
+    ('<{o"i!a,<{i<a>', 10),
+  )
+  @unpack
+  def test_num_char_clean_garbage(self, input_garbage, expected_count_chars):
+    self.assertEqual(expected_count_chars, count_valid_chars(input_garbage))
+
+
