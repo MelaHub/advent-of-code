3bd06874dd38472d351f14675ab886ba2336fcc6
Mela <pamela.gotti@instapartners.it>
2017-12-20 23:00:09 +0100

    * day10: Less swaps

--- 2017/day10/hash.py
+++ 2017/day10/hash.py
@@ -1,8 +1,11 @@
 def reverse_elements(input_list, current_position, length):
+  list_len = len(input_list)
+  if current_position >= list_len:
+    current_position -= list_len
   copy_input = [e for e in input_list]
   end_position = current_position + length
-  if end_position >= len(input_list):
-    end_position -= len(input_list)
+  if end_position >= list_len:
+    end_position -= list_len
   i = current_position - 1
   j = end_position
   start_copy = False
@@ -18,4 +21,14 @@ def reverse_elements(input_list, current_position, length):
     copy_input[i], copy_input[j] = copy_input[j], copy_input[i]
     start_copy = True
   return copy_input
-    
+  
+def reverse_for_each_length(input_list, lengths):
+  another_list = [e for e in input_list]
+  curr_position = 0
+  skip_size = 0
+  for length in lengths:
+    if length > len(input_list):
+      continue
+    another_list = reverse_elements(input_list, curr_position, length)
+    curr_position += (length + skip_size)
+    skip_size += 1
