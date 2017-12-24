43471de1d8f5a6fde4767d45b7f17e8cd2ca7a88
Mela <pamela.gotti@instapartners.it>
2017-12-24 18:26:38 +0100

    * day14: Added function that converts to binary

--- 2017/day14/defrag.py
+++ 2017/day14/defrag.py
@@ -6,3 +6,6 @@ def knot_hash_to_binary_row(knot_hash):
     binary_repr += bin(int(hex_value, 16))[2:].zfill(4)
   return binary_repr
 
+def input_to_knot(input_string):
+  return knot_hash(range(128), input_string)
+
--- 2017/day14/defrag_test.py
+++ 2017/day14/defrag_test.py
@@ -8,3 +8,6 @@ class DefragTest(unittest.TestCase):
 
   def test_knot_hash_to_binary(self):
     self.assertEquals('10100000110000100000000101110000', knot_hash_to_binary_row('a0c20170'))
+
+  def test_input_string_to_hash(self):
+    self.assertEquals('', input_to_know('flqrgnkx-0'))
