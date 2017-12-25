4a80e743fbde0314c32432b66aa54d87888ac3c5
Mela <pamela.gotti@instapartners.it>
2017-12-25 22:23:55 +0100

    * day15: Use generators and added tests or second part

--- 2017/day15/dueling.py
+++ 2017/day15/dueling.py
@@ -2,6 +2,7 @@ class Generator(object):
   
   curr_value = None
   factor = None
+  remainder_of = 2147483647
 
   def __init__(self, starting_value, factor):
     self.curr_value = starting_value
@@ -12,13 +13,11 @@ class Generator(object):
 
 class GeneratorFirstHalf(Generator):
 
-  remainder_of = 2147483647
-
   def gen_next(self):
-    self.curr_value = (self.curr_value * self.factor) % self.remainder_of
-    return self.curr_value
+    while True:
+      self.curr_value = (self.curr_value * self.factor) % self.remainder_of
+      yield self.curr_value
   
-
 class GeneratorAFirstHalf(GeneratorFirstHalf):
   def __init__(self, starting_value):
     factor = 16807
@@ -29,6 +28,33 @@ class GeneratorBFirstHalf(GeneratorFirstHalf):
     factor = 48271
     super(GeneratorBFirstHalf, self).__init__(starting_value, factor)
 
+class GeneratorSecondHalf(Generator):
+
+  divisor = None
+
+  def __init__(self, starting_value, factor, divisor):
+    self.divisor = divisor
+    super(GeneratorSecondHalf, self).__init__(starting_value, factor)
+
+  def gen_next(self):
+    while True:
+      self.curr_value = (self.curr_value * self.factor) % self.remainder_of
+      if (self.curr_value % self.divisor) == 0:
+        yield self.curr_value
+    
+
+class GeneratorASecondHalf(GeneratorSecondHalf):
+  def __init__(self, starting_value):
+    factor = 16807
+    divisor = 4
+    super(GeneratorASecondHalf, self).__init__(starting_value, factor, divisor)
+
+class GeneratorBSecondHalf(GeneratorSecondHalf):
+  def __init__(self, starting_value):
+    factor = 48271
+    divisor = 8
+    super(GeneratorBSecondHalf, self).__init__(starting_value, factor, divisor)
+
 
 class Judge(object):
 
@@ -38,14 +64,13 @@ class Judge(object):
     self.generators = list_generators
 
   def are_statuses_equal(self, values):
-    return len(set([bin(v)[-16:] for v in values])) == 1
+    return len(set([v & 0xFFFF for v in values])) == 1
 
   def judge(self, n_rounds):
     correct_rounds = 0
+    generators = [gen.gen_next() for gen in self.generators]
     for i in range(n_rounds):
-      for generator in self.generators:
-        generator.gen_next()
-      if self.are_statuses_equal([gen.curr_value for gen in self.generators]):
+      if self.are_statuses_equal([gen.next() for gen in generators]):
         correct_rounds += 1
     return correct_rounds
     
--- 2017/day15/dueling_test.py
+++ 2017/day15/dueling_test.py
@@ -9,13 +9,18 @@ class DefragTest(unittest.TestCase):
   @data(
     (65, lambda x: GeneratorAFirstHalf(x), [1092455, 1181022009, 245556042, 1744312007, 1352636452]),
     (8921, lambda x: GeneratorBFirstHalf(x), [430625591, 1233683848, 1431495498, 137874439, 285222916]),
+    (65, lambda x: GeneratorASecondHalf(x), [1352636452, 1992081072, 530830436, 1980017072, 740335192]),
+    (8921, lambda x: GeneratorBSecondHalf(x), [1233683848, 862516352, 1159784568, 1616057672, 412269392]),
   )
   @unpack
   def test_gen_next_value(self, start_value, get_generator, expected_values):
     gen = get_generator(start_value)
+    generator = gen.gen_next()
+    i = 0
     values = []
-    for i in range(0, 5):
-      values.append(gen.gen_next())
+    while i < 5:
+      values.append(generator.next())
+      i += 1
     self.assertEquals(expected_values, values)
 
   @data(
@@ -30,11 +35,14 @@ class DefragTest(unittest.TestCase):
     self.assertEquals(expected_judgment, Judge([]).are_statuses_equal(input_values))
 
   @data(
-    (65, 8921, 5, 1),
-    # (65, 8921, 40000000, 588),
-    # (277, 349, 40000000, 592),
+    ([GeneratorAFirstHalf(65), GeneratorBFirstHalf(8921)], 5, 1),
+    ([GeneratorAFirstHalf(65), GeneratorBFirstHalf(8921)], 40000000, 588),
+    ([GeneratorAFirstHalf(277), GeneratorBfirstHalf(349)], 40000000, 592),
+    ([GeneratorASecondHalf(65), GeneratorBSecondHalf(8921)], 5000000, 309),
+    ([GeneratorASecondHalf(277), GeneratorBSecondHalf(349)], 5000000, 320),
   )
   @unpack
-  def test_assert_correct_judgment(self, start_a, start_b, n_rounds, expected_rounds):
-    judge = Judge([GeneratorAFirstHalf(start_a), GeneratorBFirstHalf(start_b)])
+  def test_assert_icorrect_judgment(self, input_generators, n_rounds, expected_rounds):
+    judge = Judge(input_generators)
     self.assertEquals(expected_rounds, judge.judge(n_rounds))
+
