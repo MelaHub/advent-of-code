32955e4940eb4004fdd81255ccbffa267e3866db
Mela <pamela.gotti@instapartners.it>
2017-12-28 20:54:30 +0100

    * day19: Added second part of day 19

--- 2017/day19/tubes.py
+++ 2017/day19/tubes.py
@@ -9,61 +9,62 @@ LEFT = 'LEFT'
 RIGHT = 'RIGHT'
 EMPTY = ' '
 
+Coordinate = namedtuple('Coordinate', ['row', 'col'])
+Position = namedtuple('Position', ['coordinate', 'direction'])
+
 def find_starting_point(grid):
   for i in range(len(grid[0])):
     if grid[0][i] == VERTICAL_TUBE:
-      return (0, i), DOWN
+      return Position(Coordinate(0, i), DOWN)
   raise Exception('No starting point found')
 
 def init_tubes_from_file():
   grid = []
   with open('tubes') as f:
-    grid.append(f.read().split('\n'))
+    grid += f.read().split('\n')
   return grid
-
-Coordinate = namedtuple('Coordinate', ['row', 'col'])
-Movement = namedtuple('Movement', ['coordinate', 'direction'])
 
 def check_in_boundaries(coordinates, symbol, grid):
   return (coordinates.row >= 0 and coordinates.row < len(grid) and
     coordinates.col >= 0 and coordinates.col < len(grid[0]) and
-    grid[coordinates.row][coordinates.col] != symbol)
+    grid[coordinates.row][coordinates.col] != symbol and 
+    grid[coordinates.row][coordinates.col] != EMPTY)
 
-def move(current_movement, grid):
+def move(current_position, grid):
 
-  AlternativeMovements = namedtuple('AlternativeMovements', ['keep_going', 'first_alternative', 'second_alternative'])
+  AlternativePositions = namedtuple('AlternativePositions', ['keep_going', 'first_alternative', 'second_alternative'])
 
-  row, col = current_movement.coordinate.row, current_movement.coordinate.col
-  current_direction = current_movement.direction
+  row, col = current_position.coordinate.row, current_position.coordinate.col
+  current_direction = current_position.direction
 
   alternatives = None
   
   if current_direction == DOWN:
-    alternatives = AlternativeMovements(
-      Movement(Coordinate(row + 1, col), DOWN),
-      Movement(Coordinate(row, col - 1), LEFT),
-      Movement(Coordinate(row, col + 1), RIGHT)
+    alternatives = AlternativePositions(
+      Position(Coordinate(row + 1, col), DOWN),
+      Position(Coordinate(row, col - 1), LEFT),
+      Position(Coordinate(row, col + 1), RIGHT)
     )
   elif current_direction == UP:
-    alternatives = AlternativeMovements(
-      Movement(Coordinate(row - 1, col), UP),
-      Movement(Coordinate(row, col - 1), LEFT),
-      Movement(Coordinate(row, col + 1), RIGHT)
+    alternatives = AlternativePositions(
+      Position(Coordinate(row - 1, col), UP),
+      Position(Coordinate(row, col - 1), LEFT),
+      Position(Coordinate(row, col + 1), RIGHT)
     )
   elif current_direction == RIGHT:
-    alternatives = AlternativeMovements(
-      Movement(Coordinate(row, col + 1), RIGHT),
-      Movement(Coordinate(row - 1, col), UP),
-      Movement(Coordinate(row + 1, col), DOWN)
+    alternatives = AlternativePositions(
+      Position(Coordinate(row, col + 1), RIGHT),
+      Position(Coordinate(row - 1, col), UP),
+      Position(Coordinate(row + 1, col), DOWN)
     )
   elif current_direction == LEFT:
-    alternatives = AlternativeMovements(
-      Movement(Coordinate(row, col - 1), LEFT),
-      Movement(Coordinate(row - 1, col), UP),
-      Movement(Coordinate(row + 1, col), DOWN)
+    alternatives = AlternativePositions(
+      Position(Coordinate(row, col - 1), LEFT),
+      Position(Coordinate(row - 1, col), UP),
+      Position(Coordinate(row + 1, col), DOWN)
     )
   next_movement = None
-  if check_in_boundaries(current_movement.coordinate, CROSSROAD, grid):
+  if check_in_boundaries(current_position.coordinate, CROSSROAD, grid):
     next_movement = alternatives.keep_going
   else:
     if check_in_boundaries(alternatives.first_alternative.coordinate, EMPTY, grid):
@@ -71,4 +72,19 @@ def move(current_movement, grid):
     elif check_in_boundaries(alternatives.second_alternative.coordinate, EMPTY, grid):
       next_movement = alternatives.second_alternative
   return next_movement
-    
+
+def follow_the_tubes(grid):
+  curr_position = find_starting_point(grid)
+  letters = ''
+  number_of_steps = 0
+  while curr_position != None:
+    number_of_steps += 1
+    curr_tube = grid[curr_position.coordinate.row][curr_position.coordinate.col]
+    if curr_tube not in [CROSSROAD, VERTICAL_TUBE, HORIZONTAL_TUBE, EMPTY]:
+      letters += curr_tube
+    curr_position = move(curr_position, grid)
+  return letters, number_of_steps - 1
+
+def follow_the_tubes_from_file():
+  grid = init_tubes_from_file()
+  return follow_the_tubes(grid)
--- 2017/day19/tubes_test.py
+++ 2017/day19/tubes_test.py
@@ -16,25 +16,27 @@ class TubesTest(unittest.TestCase):
   ]
 
   def test_find_starting_point(self):
-    self.assertEquals(((0, 4), DOWN), find_starting_point(self.TEST_GRID))
+    self.assertEquals(Position(Coordinate(0, 4), DOWN), find_starting_point(self.TEST_GRID))
 
   @data(
-    (Movement(Coordinate(0, 4), DOWN), Movement(Coordinate(1, 4), DOWN)),
-    (Movement(Coordinate(5, 4), DOWN), Movement(Coordinate(5, 5), RIGHT)),
-    (Movement(Coordinate(5, 7), DOWN), Movement(Coordinate(5, 6), LEFT)),
-    (Movement(Coordinate(5, 5), RIGHT), Movement(Coordinate(5, 6), RIGHT)),
-    (Movement(Coordinate(5, 7), RIGHT), Movement(Coordinate(4, 7), UP)),
-    (Movement(Coordinate(1, 10), RIGHT), Movement(Coordinate(2, 10), DOWN)),
-    (Movement(Coordinate(4, 7), UP), Movement(Coordinate(3, 7), UP)),
-    (Movement(Coordinate(3, 13), UP), Movement(Coordinate(3, 12), LEFT)),
-    (Movement(Coordinate(1, 7), UP), Movement(Coordinate(1, 8), RIGHT)),
-    (Movement(Coordinate(5, 6), LEFT), Movement(Coordinate(5, 5), LEFT)),
-    (Movement(Coordinate(5, 4), LEFT), Movement(Coordinate(4, 4), UP)),
-    (Movement(Coordinate(1, 7), LEFT), Movement(Coordinate(2, 7), DOWN)),
+    (Position(Coordinate(0, 4), DOWN), Position(Coordinate(1, 4), DOWN)),
+    (Position(Coordinate(5, 4), DOWN), Position(Coordinate(5, 5), RIGHT)),
+    (Position(Coordinate(5, 7), DOWN), Position(Coordinate(5, 6), LEFT)),
+    (Position(Coordinate(5, 5), RIGHT), Position(Coordinate(5, 6), RIGHT)),
+    (Position(Coordinate(5, 7), RIGHT), Position(Coordinate(4, 7), UP)),
+    (Position(Coordinate(1, 10), RIGHT), Position(Coordinate(2, 10), DOWN)),
+    (Position(Coordinate(4, 7), UP), Position(Coordinate(3, 7), UP)),
+    (Position(Coordinate(3, 13), UP), Position(Coordinate(3, 12), LEFT)),
+    (Position(Coordinate(1, 7), UP), Position(Coordinate(1, 8), RIGHT)),
+    (Position(Coordinate(5, 6), LEFT), Position(Coordinate(5, 5), LEFT)),
+    (Position(Coordinate(5, 4), LEFT), Position(Coordinate(4, 4), UP)),
+    (Position(Coordinate(1, 7), LEFT), Position(Coordinate(2, 7), DOWN)),
   )
   @unpack
   def test_move_in_space(self, current_coordinates, next_coordinate):
     self.assertEquals(next_coordinate, move(current_coordinates, self.TEST_GRID))
 
-    
+  def test_follow_tubes(self):
+    self.assertEquals(('ABCDEF',38), follow_the_tubes(self.TEST_GRID))
+    self.assertEquals(('GPALMJSOY', 16204), follow_the_tubes_from_file())
 
