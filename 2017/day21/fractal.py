36f14f073c3ed556666b2078aa43e21e68ddb555
Mela <pamela.gotti@instapartners.it>
2017-12-30 11:59:36 +0100

    * day21: First half on day 21 done

--- 2017/day21/fractal.py
+++ 2017/day21/fractal.py
@@ -1,3 +1,4 @@
+import math
 STARTING_SQUARE = [
   '.#.',
   '..#',
@@ -9,17 +10,26 @@ def rotate_flip_square(input_square):
   rotate90 = rotatezip(input_square)
   rotate180 = rotatezip(rotate90)
   rotate270 = rotatezip(rotate180)
-  return [input_square, rotate90, rotate180, rotate270, [row[::-1] for row in input_square]]
+  v_flip = [row[::-1] for row in input_square]
+  h_flip = [row for row in input_square[::-1]]
+  v_h_flip = [row[::-1] for row in h_flip]
+  transpose_single = [input_square[j][i] for i in range(len(input_square) - 1, -1, -1) for j in range(len(input_square) - 1, -1, -1)]
+  transpose = [''.join(transpose_single[k:k+len(input_square)]) for k in range(0, len(transpose_single), len(input_square))]
+  return [input_square, rotate90, rotate180, rotate270, v_flip, h_flip, v_h_flip, transpose, [row for row in rotate270[::-1]]]
 
 def replace_square(input_square, sub_rules):
-  rotated_flipped_input = rotate_flip_square(input_square)
-  matching_output = None
-  for square in rotated_flipped_input:
-    match = sub_rules.get('/'.join(square))
-    if match is not None:
-      matching_output = match
-      break
-  return matching_output.split('/')
+  match = sub_rules.get('/'.join(input_square))
+  if match is not None:
+    return match.split('/')
+  raise Exception('Couldn\'t find a match fo %s' % '/'.join(input_square))
+
+def enrich_rules(input_rules):
+  enriched_rules = {i: j for i, j in input_rules.iteritems()}
+  for key, value in enriched_rules.items():
+    flipped_keys = rotate_flip_square(key.split('/'))
+    for flipped_key in flipped_keys:
+      enriched_rules['/'.join(flipped_key)] = value
+  return enriched_rules
 
 def split_by_factor(input_square, factor):
   output_rows = []
@@ -30,19 +40,47 @@ def split_by_factor(input_square, factor):
 
 def split_square(input_square):
   if len(input_square) == 2 or len(input_square) == 3:
-    return input_square
+    return [input_square]
   if len(input_square) % 2 == 0:
     return split_by_factor(input_square, 2)
   elif len(input_square) % 3 == 0:
     return split_by_factor(input_square, 3)
   return Exception('HEEEELP a weird square!!!')
 
+def merge(squares):
+  if len(squares) == 1:
+    return squares[0]
+  square_size = math.sqrt(len(squares) * len(squares[0] * len(squares[0][0])))
+  blocks_per_row = int(square_size / len(squares[0][0]))
+  grouped_blocks = zip(*[squares[i:i+blocks_per_row] for i in range(0, len(squares), blocks_per_row)])
+  merge_grouped_blocks = []
+  for group in grouped_blocks:
+    merge_group = []
+    for g in group:
+      merge_group += g
+    merge_grouped_blocks.append(merge_group)
+  output_square = [''.join(group) for group in zip(*merge_grouped_blocks)]
+  return output_square
+
 def draw_fractal(input_square, round_left, sub_rules):
+  enriched_rules = enrich_rules(sub_rules)
   if round_left == 0:
     return input_square
-  new_square = replace_square(input_square, sub_rules)
-  sub_squares = split_square(new_square)
-  return [draw_fractal(sub_square, round_left - 1, sub_rules) for sub_square in sub_squares]
+  sub_squares = split_square(input_square)
+  new_squares = [replace_square(square, enriched_rules) for square in sub_squares]
+  merged_squares = merge(new_squares)
+  return draw_fractal(merged_squares, round_left - 1, sub_rules)
  
-def count_pixels_on(input_square, round_left, sub_rules):
-  return len([c for square in draw_fractal(input_square, round_left, sub_rules) for row in square for c in row if c == '#']) 
+def count_pixels_on(round_left, sub_rules):
+  a = draw_fractal(STARTING_SQUARE, round_left, sub_rules)
+  return len([c for square in draw_fractal(STARTING_SQUARE, round_left, sub_rules) for row in square for c in row if c == '#']) 
+
+def rules_from_file():
+  rules = {}
+  with open('fractal') as f:
+    rules = {i.strip(): j.strip() for i, j in [row.split('=>') for row in f.readlines()]}
+  return rules
+
+def count_pixels_on_from_file(number_of_rounds):
+  return count_pixels_on(number_of_rounds, rules_from_file())
+
--- 2017/day21/fractal_test.py
+++ 2017/day21/fractal_test.py
@@ -12,25 +12,66 @@ class FractalTest(unittest.TestCase):
     '.#./..#/###': '#..#/..../..../#..#'
   }
 
+  def test_enrich_rules(self):
+    expected_rules = {
+      '../.#': '##./#../...',
+      '../#.': '##./#../...',
+      '.#/..': '##./#../...', 
+      '.#/..': '##./#../...', 
+      '#./..': '##./#../...', 
+      '../.#': '##./#../...', 
+      '#./..': '##./#../...', 
+      '.#/..': '##./#../...', 
+      '../#.': '##./#../...', 
+      '../#.': '##./#../...', 
+      '../.#': '##./#../...', 
+      '#./..': '##./#../...',
+      '.#./..#/###': '#..#/..../..../#..#', 
+      '.#./#../###': '#..#/..../..../#..#', 
+      '###/..#/.#.': '#..#/..../..../#..#', 
+      '.##/#.#/..#': '#..#/..../..../#..#', 
+      '##./#.#/#..': '#..#/..../..../#..#',
+      '..#/#.#/.##': '#..#/..../..../#..#',
+      '###/#../.#.': '#..#/..../..../#..#',
+      '###/..#/.#.': '#..#/..../..../#..#',
+      '.#./#../###': '#..#/..../..../#..#',
+      '#../#.#/##.': '#..#/..../..../#..#',
+      '..#/#.#/.##': '#..#/..../..../#..#',
+      '##./#.#/#..': '#..#/..../..../#..#',
+    }
+    enriched_rules = enrich_rules(expected_rules)
+    self.assertEquals(sorted(expected_rules.keys()), sorted(enriched_rules.keys()))
+    for key, value in expected_rules.iteritems():
+      self.assertEquals(value, enriched_rules.get(key))
+
   @data(
     (STARTING_SQUARE, TEST_RULES['.#./..#/###'].split('/')),
     (['#.', '..'], TEST_RULES['../.#'].split('/')),
   )
   @unpack
   def test_parse_input(self, input_square, expected_output):
-    self.assertEquals(expected_output, replace_square(input_square, self.TEST_RULES))
-
+    self.assertEquals(expected_output, replace_square(input_square, enrich_rules(self.TEST_RULES)))
+  
   @data(
-    (['12', '34'], [['12', '34'], ['31', '42'], ['43', '21'], ['24', '13'], ['21', '43']]), 
-    (['123', '456', '789'], [['123', '456', '789'], ['741', '852', '963'], ['987','654','321'], ['369','258','147'], ['321', '654', '987']]),
+    (['12', '34'], [['12', '34'], ['31', '42'], ['43', '21'], ['24', '13'], ['21', '43'], ['34', '12'], ['43', '21'], ['42', '31'], ['13', '24']]), 
+    (['123', '456', '789'], [['123', '456', '789'], ['741', '852', '963'], ['987','654','321'], ['369','258','147'], ['321', '654', '987'], ['789', '456', '123'], ['987', '654', '321'], ['963', '852', '741'], ['147', '258', '369']]),
   )
   @unpack
   def test_rotate_flip_square(self, input_square, expected_squares):
     self.assertEquals(expected_squares, rotate_flip_square(input_square))
 
   @data(
-    (STARTING_SQUARE, STARTING_SQUARE),
-    (['..', '.#'], ['..', '.#']),
+    ([['12', '34'], ['56', '78'], ['90', 'AB'], ['CD', 'EF']], ['1256', '3478', '90CD', 'ABEF']),
+    ([['111', '222', '333'], ['444', '555', '666'], ['777', '888', '999'], ['000', 'AAA', 'BBB']], ['111444', '222555', '333666', '777000', '888AAA', '999BBB']),
+  )
+  @unpack
+  def test_merge(self, input_squares, expected_square):
+    self.assertEquals(expected_square, merge(input_squares))
+
+
+  @data(
+    (STARTING_SQUARE, [STARTING_SQUARE]),
+    (['..', '.#'], [['..', '.#']]),
     (['#..#', '....', '....', '#..#'], [['#.', '..'], ['.#', '..'], ['..', '#.'], ['..', '.#']]),
     (['##.##....', '##.....##', '#.#.#.#.#', '.#.#.#.#.', '.#...#...', '.........', '..##..##.', '#.....#..', '.#.##....'], [['##.', '##.', '#.#'], ['##.', '...', '.#.'], ['...', '.##', '#.#'], ['.#.', '.#.', '...'], ['#.#', '..#', '...'], ['.#.', '...', '...'], ['..#', '#..', '.#.'], ['#..', '...', '##.'], ['##.', '#..', '...']]),
   )
@@ -39,5 +80,6 @@ class FractalTest(unittest.TestCase):
     self.assertEquals(expected_squares, split_square(input_square))
 
   def test_draw_fractal(self):
-    self.assertEquals([['##.', '#..', '...'], ['##.', '#..', '...'], ['##.', '#..', '...'], ['##.', '#..', '...']], draw_fractal(STARTING_SQUARE, 2, self.TEST_RULES))
-    self.assertEquals(12, count_pixels_on(STARTING_SQUARE, 2, self.TEST_RULES))
+    self.assertEquals(['##.##.', '#..#..', '......', '##.##.', '#..#..', '......'], draw_fractal(STARTING_SQUARE, 2, self.TEST_RULES))
+    self.assertEquals(12, count_pixels_on(2, self.TEST_RULES))
+    self.assertEquals(208, count_pixels_on_from_file(5))
