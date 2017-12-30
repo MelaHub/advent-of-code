6ccf099032ba68998ae34c9723348264c18e9314
Mela <pamela.gotti@instapartners.it>
2017-12-30 23:33:10 +0100

    * day22: Added test case for part 2

--- 2017/day22/virus_test.py
+++ 2017/day22/virus_test.py
@@ -86,9 +86,9 @@ class VirusTest(unittest.TestCase):
 
   def test_move_super_virus(self):
     grid = init_grid(self.TEST_MAP, SuperVirus)
-    self.assertEquals(set([(2, 0), (0, 1)]), grid.infected_cells)
-    grid.move_virus(1)
-    # self.assertEquals(26, grid.number_of_caused_infections)
+    self.assertEquals(set([(2, 0), (0, 1)]), self.select_only_infected(grid))
+    grid.move_virus(100)
+    self.assertEquals(26, grid.number_of_caused_infections)
 
   # def test_move_super_virus_from_file(self):
   #   input_grid = get_grid_from_file()
