25af7512bfa77a3b8a6d6a6ac857b9a49d68cd23
MelaHub <pamela.gotti@gmail.com>
2020-12-06 19:16:41 +0100

    * 2020: Two phases passport

--- 2020/day4/src/main.rs
+++ 2020/day4/src/main.rs
@@ -6,7 +6,39 @@ use regex::Regex;
 
 const PASSPORT_REGEX: &str = r"([^ ]+):([^ ]+)";
 
-fn read_input() -> Vec<HashMap<String, String>> {
+// fn validate_birth_year(year: String): Result<usize, ParseIntError> {
+
+// }
+// byr (Birth Year) - four digits; at least 1920 and at most 2002.
+// iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+// eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+// hgt (Height) - a number followed by either cm or in:
+// If cm, the number must be at least 150 and at most 193.
+// If in, the number must be at least 59 and at most 76.
+// hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+// ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+// pid (Passport ID) - a nine-digit number, including leading zeroes.
+// cid (Country ID) - ignored, missing or not.
+
+// struct Passport {
+//     byr: usize,
+//     iyr: usize,
+//     eyr: usize,
+//     hgt: str,
+//     hcl: str,
+//     ecl: str,
+//     pid: str,
+// }
+
+// impl Passport {
+//     type Err = ParsePassportError;
+
+//     fn from_hash(input_hash: &HashMap<String, String>) -> Result<Passport, Self::Err> {
+
+//     }
+// }
+
+fn read_input() -> Vec<String> {
     let input_lines: Vec<String> = stdin()
         .lock()
         .lines()
@@ -14,24 +46,39 @@ fn read_input() -> Vec<HashMap<String, String>> {
         .filter_map(|line| line.parse().ok())       
         .collect();
 
-    let mut currHash: HashMap<String, String> = HashMap::new();
+    let mut passports_as_str: Vec<String> = Vec::new();
+
+    let mut curr_passport: String = String::from("");
+
+    for line in input_lines {
+        if line.chars().count() == 0 {
+            passports_as_str.push(curr_passport);
+            curr_passport = String::from("");
+        }
+        curr_passport.push_str(" ");
+        curr_passport.push_str(&line);
+    }
+
+    passports_as_str.push(curr_passport);
+
+    passports_as_str
+}
+
+fn parse_passport(passports_as_str: Vec<String>) -> Vec<HashMap<String, String>> {
+
     let mut passports: Vec<HashMap<String, String>> = Vec::new();
 
     lazy_static! {
         static ref REGEX: Regex = Regex::new(PASSPORT_REGEX).unwrap();
       }
     
-    for line in input_lines {
-        if line.chars().count() == 0 {
-          passports.push(currHash);
-          currHash = HashMap::new();
-        }
-        for cap in REGEX.captures_iter(&line) {
+    for passport in passports_as_str {
+        let mut currHash: HashMap<String, String> = HashMap::new();
+        for cap in REGEX.captures_iter(&passport) {
             currHash.insert(String::from(&cap[1]), String::from(&cap[2]));
         }
+        passports.push(currHash);
     }
-    passports.push(currHash);
-    currHash = HashMap::new();
 
     passports                               
 }
@@ -63,7 +110,8 @@ fn main() {
 
     println!("Passport validation 🕵️‍♀️");
     
-    let input_passports = read_input();
+    let input_passports_as_str = read_input();
+    let input_passports = parse_passport(input_passports_as_str);
     let npassports = input_passports.len();
     
     let mandatory_keys = get_mandatory_keys();
