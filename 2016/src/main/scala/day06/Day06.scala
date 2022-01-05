3cfc0e527b7fbfc65384d722663aaa261a9f15eb
MelaHub <pamela.gotti@gmail.com>
2022-01-05 15:05:22 +0100

    * 2016-day06: Added functions to decode the message

---
+++ 2016/src/main/scala/day06/Day06.scala
@@ -0,0 +1,29 @@
+package day06
+
+object Day06 extends App {
+
+  def decomposedValues: List[String] => List[List[Char]] = messages => messages
+    .foldLeft(List.empty[List[Char]]){
+      case (currBag, message) =>
+        message.zipAll(currBag, '-', List.empty[Char])
+          .map {
+            case (currChar, currBag) => currBag :+ currChar
+          }.toList
+    }
+
+  def counter: List[List[Char]] => List[Map[Char, Int]] = values => values
+    .map(
+      _.groupBy(identity)
+        .map {
+          case (currChar, listChars) => currChar -> listChars.size
+        })
+
+  def mostCommonLetters: List[Map[Char, Int]] => List[Option[Char]] = seqCounters => seqCounters
+    .map(
+      _.toList.sortBy { case (_, count) => -count }.take(1).headOption.map { case (currChar, _) => currChar}
+    )
+
+  def findHiddenMessage: List[String] => String = messages => (decomposedValues andThen counter andThen mostCommonLetters)(messages).flatten.mkString
+
+
+}
